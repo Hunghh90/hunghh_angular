f2b2790cb9f14ee82c94d7c99b66c7bc85a01379
Hunghh90 <hungbkvn9x1@gmail.com>
2022-06-17 15:17:05 +0700

    Assignment2

--- src/app/assignment2.component/assignment2.component.ts
+++ src/app/assignment2.component/assignment2.component.ts
@@ -1,9 +1,17 @@
 import {Component} from "@angular/core";
+import {IAssignment2} from "../interfaces/assignment2.interface";
+
 
 @Component({
-  selector: 'app-assignment2',
-  templateUrl: './assignment2.component.html',
+  selector:'app-assignment2',
+  templateUrl:'./assignment2.component.html',
 })
 export class Assignment2Component{
+  contents:  IAssignment2[]=[
+    {name:'To Do', task: 'Task 1',noidung:'noidung1'},
+    {name:'In Progess', task: 'Task 2',noidung:'noidung2'},
+    {name:'In Review', task: 'Task 3',noidung:'noidung3'},
+    {name:'Done', task: 'Task 4',noidung:'noidung4'},
 
+  ]
 }
--- src/app/interfaces/assignment2.interface.ts
+++ src/app/interfaces/assignment2.interface.ts
@@ -1,4 +1,5 @@
 export interface IAssignment2{
   name: string,
   noidung: string,
+  task: string,
 }
