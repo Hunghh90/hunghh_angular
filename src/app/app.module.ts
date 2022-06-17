5fef4758bc0095ee064bd0746c1cf122a994e053
Hunghh90 <hungbkvn9x1@gmail.com>
2022-06-17 22:05:29 +0700

    up code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {ThuchanhangularComponent} from "./thuchanhangular/thuchanhangular.component";
+import {HttpClientModule} from "@angular/common/http";
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import {ThuchanhangularComponent} from "./thuchanhangular/thuchanhangular.compon
     ThuchanhangularComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/thuchanhangular/thuchanhangular.component.ts
+++ src/app/thuchanhangular/thuchanhangular.component.ts
@@ -1,5 +1,5 @@
 import {Component} from "@angular/core";
-import {IThuchanh, Category, Mail} from "../interfaces/thuchanh.interface";
+import {Category, Mail} from "../interfaces/thuchanh.interface";
 import {DataService} from "../services/data.service";
 
 @Component({
@@ -10,12 +10,23 @@ export class ThuchanhangularComponent{
 
   name: Category[] = [];
   contacts: Mail[]=[];
-  constructor(private DataService : DataService) {
+  constructor(private dataService : DataService) {
   }
   ngOnInit(){
-    this.DataService.contaclist().subscribe(value=>{
+    this.dataService.contaclist().subscribe(value=>{
       this.name = value.data.categories;
       this.contacts = value.data.mails;
+    })
+  }
+  changeMails(category: string, child: string){
+    var new_list: Mail[] = [];
+    this.dataService.contaclist().subscribe(value=>{
+      for(var i=0;i<value.data.mails.length;i++){
+        if(value.data.mails[i].child == child && value.data.mails[i].child == child){
+          new_list.push(value.data.mails[i]);
+        }
+      }
+      this.contacts = new_list;
     })
   }
 
